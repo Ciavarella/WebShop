62acbfc3f0363c25c623195f1e1989b7d64d21fc
Victor Ciavarella <victor.ciavarella@gmail.com>
2021-02-17 21:39:29 +0100

    Add spinner for when page is loading

---
+++ src/Components/Spinner.tsx
@@ -0,0 +1,11 @@
+import '../Styles/Spinner.css';
+
+const Spinner = (): JSX.Element => {
+  return (
+    <div className="loader-container">
+      <div className="loader" />
+    </div>
+  )
+};
+
+export default Spinner;

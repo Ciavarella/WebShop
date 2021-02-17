9ce0d78f5e64dc0c24bdc0179744d4f3c511dde6
Victor Ciavarella <victor.ciavarella@gmail.com>
2021-02-17 21:38:04 +0100

    Add header

---
+++ src/Components/Header.tsx
@@ -0,0 +1,12 @@
+import '../Styles/Header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <p>Home</p>
+      <p>Checkout</p>
+    </div>
+  );
+}
+
+export default Header;

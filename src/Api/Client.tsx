294efffdf2953dd29bf17112a2000d5989e896c9
Victor Ciavarella <victor.ciavarella@gmail.com>
2021-02-18 23:58:40 +0100

    Add method getItemById

--- src/Api/Client.tsx
+++ src/Api/Client.tsx
@@ -4,8 +4,13 @@ const mockData: Array<Item> = MockData.items
 
 const getAllItems = async (): Promise <Array<Item>> =>
   new Promise<Array<Item>>((resolve) => {
-  setTimeout(() => resolve(mockData), 2000);
+  setTimeout(() => resolve(mockData), 1500);
 });
 
+const getItemById = async (id: Number): Promise<Item> =>
+  new Promise<Item>((resolve, reject) => {
+  const res: Item | undefined = mockData.find(obj => obj.id === id);
+  setTimeout(() => res ? resolve(res) : reject('Could not find product'), 1500);
+});
 
-export { getAllItems }
+export { getAllItems, getItemById }

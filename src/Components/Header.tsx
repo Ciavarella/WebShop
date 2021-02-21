6fa4315d4c8505a6584825aed5d68a8f3435dd30
Victor Ciavarella <victor.ciavarella@gmail.com>
2021-02-21 16:28:53 +0100

    WIP Add route and number in header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,13 +1,27 @@
 import '../Styles/Header.css';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react'
 
 const Header = (): JSX.Element => {
+  const [Amount, SetAmount] = useState<any>(0);
+  
+  useEffect(() => {
+    SetAmount(0);
+  }, [])
+
   return (
     <div className="header">
       <Link to="/">
         <p>Home</p>
       </Link>
-      <p>Checkout</p>
+      <Link to="/checkout">
+        <div className="checkout">
+          <div className="amount-container">
+            <span>{Amount}</span>
+          </div>
+          <p>Checkout</p>
+        </div>
+      </Link>
     </div>
   );
 }

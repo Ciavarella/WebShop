9d2c7b4dcc0847e3da5f3211feae1adef1430d5b
Victor Ciavarella <victor.ciavarella@gmail.com>
2021-02-24 22:23:04 +0100

    remove log and update header styles

--- src/Api/Client.tsx
+++ src/Api/Client.tsx
@@ -3,19 +3,18 @@ import { Item } from './types';
 const mockData: Array<Item> = MockData.items
 
 const getAllItems = async (): Promise <Array<Item>> =>
-  new Promise<Array<Item>>((resolve) => {
+new Promise<Array<Item>>((resolve) => {
   setTimeout(() => resolve(mockData), 1500);
 });
 
 const getItemById = async (id: Number): Promise<Item> =>
-  new Promise<Item>((resolve, reject) => {
+new Promise<Item>((resolve, reject) => {
   const res: Item | undefined = mockData.find(obj => obj.id === id);
   setTimeout(() => res ? resolve(res) : reject('Could not find product'), 1500);
 });
 
 const addItemToCart = async (amount: number, extra: any, id: Number) => {
   const item = mockData.filter(e => e.id === id);
-  console.log("item", item);
   return new Promise((resolve) => {
     setTimeout(() => resolve(item), 1500); 
   });
